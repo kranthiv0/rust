d91929280b9ab6e106fc510a28a180dc64b85ee4
kranthi <kranthigowthamkumar@gmail.com>
2024-01-04 19:41:48 +0530

    initial commit

--- test1/target/debug/deps/test1-71113c32d4bf0385.d
+++
@@ -1,5 +0,0 @@
-C:\Users\krant\projects\rust\test1\target\debug\deps\libtest1-71113c32d4bf0385.rmeta: src\main.rs
-
-C:\Users\krant\projects\rust\test1\target\debug\deps\test1-71113c32d4bf0385.d: src\main.rs
-
-src\main.rs:
--- test1/target/debug/deps/test1-d878d2e4c425537e.d
+++
@@ -1,5 +0,0 @@
-C:\Users\krant\projects\rust\test1\target\debug\deps\libtest1-d878d2e4c425537e.rmeta: src\main.rs
-
-C:\Users\krant\projects\rust\test1\target\debug\deps\test1-d878d2e4c425537e.d: src\main.rs
-
-src\main.rs:
