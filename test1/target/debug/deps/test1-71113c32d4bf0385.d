a4d44daba0dcdf469a0f2d1992e85b61ecb54a5c
kranthi <kranthigowthamkumar@gmail.com>
2024-01-04 19:36:24 +0530

    test1

---
+++ test1/target/debug/deps/test1-71113c32d4bf0385.d
@@ -0,0 +1,5 @@
+C:\Users\krant\projects\rust\test1\target\debug\deps\libtest1-71113c32d4bf0385.rmeta: src\main.rs
+
+C:\Users\krant\projects\rust\test1\target\debug\deps\test1-71113c32d4bf0385.d: src\main.rs
+
+src\main.rs:
---
+++ test1/target/debug/deps/test1-d878d2e4c425537e.d
@@ -0,0 +1,5 @@
+C:\Users\krant\projects\rust\test1\target\debug\deps\libtest1-d878d2e4c425537e.rmeta: src\main.rs
+
+C:\Users\krant\projects\rust\test1\target\debug\deps\test1-d878d2e4c425537e.d: src\main.rs
+
+src\main.rs:
