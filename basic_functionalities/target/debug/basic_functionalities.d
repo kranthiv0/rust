fb755c9b71560de9b9a2e4abd516a3666a5a7985
kranthi <kranthigowthamkumar@gmail.com>
2024-01-04 23:48:50 +0530

    Hiding target

---
+++ basic_functionalities/target/debug/basic_functionalities.d
@@ -0,0 +1,1 @@
+C:\Users\krant\projects\rust\basic_functionalities\target\debug\basic_functionalities.exe: C:\Users\krant\projects\rust\basic_functionalities\src\main.rs
---
+++ basic_functionalities/target/debug/deps/basic_functionalities.d
@@ -0,0 +1,5 @@
+C:\Users\krant\projects\rust\basic_functionalities\target\debug\deps\basic_functionalities.exe: src\main.rs
+
+C:\Users\krant\projects\rust\basic_functionalities\target\debug\deps\basic_functionalities.d: src\main.rs
+
+src\main.rs:
